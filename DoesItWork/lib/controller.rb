094afcb224d6a3fa0b5b75fa47c2899c71121644
prometheus <prometheus@prometheuss-MBP.home>
2019-01-17 14:56:32 -0500

    Phone.rb has ability to reconize user input, Contry.rb can now compare bands, controller currently interacting with 3 orbjects

--- DoesItWork/lib/controller.rb
+++ DoesItWork/lib/controller.rb
@@ -7,18 +7,28 @@ require_relative '../config/environment.rb'
 class Controller
 # get the arrays from scapper and
 def initialize
-  @app = Greeter.new
+    @app = Greeter.new
 end
 
 def call
-@app.greet
-self.answer
+    @app.greet
+    self.answer
+    phone =Phone.new(@app.phone_choice)
+    phone.bands = [2,3,4]
+    contry = Contry.new(name:"Brazil", bands:[2,3,4,28,], carriers:"vivo")
+    @missing= contry.answer(phone.bands)
+    self.answer
+
 end
 
 
 def answer
-  puts "this is the user phone selection #{@app.phone_choice}"
-  puts "this is the user contry selection #{@app.contry_choice}"
+    # need to be the hearth of the application. will comprater the stats and
+    # let us know if the bands match or not.
+    puts "this is the user phone selection  #{@app.phone_choice}"
+    puts "this is the user contry selection #{@app.contry_choice}"
+    puts "this is the missing bands #{@missing}"
+
 end
 
 end
