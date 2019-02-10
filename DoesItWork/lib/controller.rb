5c4206ad17d77a7cb2e9df5edb631555dfa4de61
prometheus <prometheus@prometheuss-MacBook-Pro.local>
2019-02-09 19:06:02 -0500

    added better interface to greeter file, also fix the grammar error of contry to country

--- DoesItWork/lib/controller.rb
+++ DoesItWork/lib/controller.rb
@@ -1,34 +1,77 @@
 
 require_relative '../config/environment.rb'
 # greet user through the Greeter instance
-# grab the bands of contry and phone than compare them
+# grab the bands of country and phone than compare them
 # list
 
 class Controller
 # get the arrays from scapper and
+
 def initialize
     @app = Greeter.new
 end
 
 def call
+  # greet user, intantiate a phone, and country, have country calulate what missing
+  # .. than inform the user of the result.
+
     @app.greet
-    self.answer
-    phone =Phone.new(@app.phone_choice)
-    phone.bands = [2,3,4]
-    contry = Contry.new(name:"Brazil", bands:[2,3,4,28,], carriers:"vivo")
-    @missing= contry.answer(phone.bands)
-    self.answer
+    @phone =Phone.new(@app.phone_choice)
+    @country = Country.new(@app.country_choice)
+    @missing = @country.answer(@phone.bands)
+    sleep(2)
+    puts "Gathering more info..."
+    sleep(2)
+    self.more_info
+end
 
+#name:"Brazil", bands:[2,3,4,28,], carriers:"vivo"
+def more_info
+  # show the user a list of the bands of both the phone and country_bands
+  if @missing.empty?
+    puts "                                                                "
+    puts "LTE bands of your phone"
+    puts "=============================================================="
+    puts "Device: #{@phone.name}"
+    puts "Bands: #{@phone.bands.join(', ')}."
+    puts "                                                                "
+    puts "                                                                "
+
+    puts "LTE bands of your country"
+    puts "=============================================================="
+    puts "Country: #{@country.name.capitalize}"
+    puts "Bands: #{@country.bands.join(', ')}."
+    puts "=============================================================="
+    puts "                                                                "
+  else
+     puts "                                                                "
+     puts "LTE bands of your phone"
+     puts "=============================================================="
+     puts "Device: #{@phone.name}"
+     puts "Bands: #{@phone.bands.join(', ')}."
+     puts "=============================================================="
+
+     puts "                                                                "
+     puts "                                                                "
+
+     puts "LTE bands of your country"
+     puts "=============================================================="
+     puts "Country: #{@country.name.capitalize}"
+     puts "Bands: #{@country.bands.join(', ')}."
+     puts "=============================================================="
+     puts "                                                                "
+     puts "                                                                "
+
+     puts "=============================================================="
+     puts "Your Phone is missing Band #{@missing.join(', ')}."
+     puts "=============================================================="
+     puts "                                                                "
+
+
+  end
 end
 
 
-def answer
-    # need to be the hearth of the application. will comprater the stats and
-    # let us know if the bands match or not.
-    puts "this is the user phone selection  #{@app.phone_choice}"
-    puts "this is the user contry selection #{@app.contry_choice}"
-    puts "this is the missing bands #{@missing}"
 
-end
 
 end
---
+++ DoesItWork/lib/scrapper.rb
@@ -0,0 +1,63 @@
+
+
+class Scrapper
+  #find the bands for iphone XS , Max, and R
+  # todo : need to have a why to raise errors when whe have an invalid country choice
+  def self.scrape_xs
+  @@index_page = Nokogiri::HTML(open('https://www.apple.com/iphone/LTE/'))
+  iPhone_bands = @@index_page.css('div.cellular-table-cell')
+  goal = iPhone_bands.css('div.cellular-table-contents')
+  goal2 = goal[1]
+  @@xs_xsmax_bands = []
+  goal2.children[1].css('span').each do|items|
+  @@xs_xsmax_bands << items.text.to_i
+  end
+
+@@xs_xsmax_bands
+
+end
+
+
+
+
+  def self.scrape_xr
+    @@index_page = Nokogiri::HTML(open('https://www.apple.com/iphone/LTE/#iphone-xr'))
+    iPhone_bands = @@index_page.css('div.section-content')
+    level1 = iPhone_bands.css('div.cellular-table-contents')
+    level2 = level1[1]
+    @@xr_bands = []
+    level2.children[1].css('span').each do|items|
+      @@xr_bands << items.text.to_i
+      end
+@@xr_bands
+  end
+
+
+
+
+ def self.scrape_country (name_of_country)
+        # scrape the bands from the frequencycheck site
+        #todo : need to be aware if search results returns nil.
+     url = "https://www.frequencycheck.com/countries/#{name_of_country}"
+     @@country_bands = []
+     @@index_page = Nokogiri::HTML(open(url))
+     country_bands = @@index_page.css('div.col-md-8')
+     level1 = country_bands.css('table.table.table-hover.table-striped.table-bordered')
+     level2 = level1[3].css('a')
+
+     level2.each do |item|
+          text = item.text
+          alfanumeric_data = text.match(/(B)../)
+          if alfanumeric_data == nil
+              next
+          else
+          @@band = alfanumeric_data[0].match(/\d{1,}/)
+          @@country_bands << @@band[0].to_i
+          end
+      end
+      @@country_bands
+end
+
+
+
+  end
